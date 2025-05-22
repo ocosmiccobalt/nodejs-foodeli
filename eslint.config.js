d9f1e5a72fd816d9a32fa3e6afd70db2c19154f6
Olga Bazhenova <ocosmiccobalt@gmail.com>
2025-05-23 00:30:04 +0300

    Use single quotation marks in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 
 export default defineConfig([
-  { ignores: ["**/mongosh-crud-operations"] },
+  { ignores: ['**/mongosh-crud-operations'] },
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     rules: {
-			"no-unused-vars": "warn",
+			'no-unused-vars': 'warn',
 		},
   },
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: { globals: globals.node }
   },
 ]);
