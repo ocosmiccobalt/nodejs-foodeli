70f12669bf5ada88b99fecb4c95e663503fc6f91
Olga Bazhenova <ocosmiccobalt@gmail.com>
2025-08-16 14:47:55 +0300

    Update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,26 @@
 import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
-
 
 export default defineConfig([
   { ignores: ['**/mongosh-crud-operations'] },
   {
     files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
+    plugins: { js, '@stylistic': stylistic },
+    extends: ['js/recommended', stylistic.configs.recommended],
     rules: {
-			'no-unused-vars': 'warn',
-		},
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      '@stylistic/comma-dangle': ['error', 'never'],
+      '@stylistic/indent': ['error', 2, { offsetTernaryExpressions: false }],
+      '@stylistic/operator-linebreak': ['error', 'after'],
+      '@stylistic/semi': ['error', 'always', { omitLastInOneLineBlock: true }],
+      'no-unused-vars': 'warn'
+    }
   },
   {
     files: ['**/*.{js,mjs,cjs}'],
     languageOptions: { globals: globals.node }
-  },
+  }
 ]);
